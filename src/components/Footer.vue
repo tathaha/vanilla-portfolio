c089974dec8ce8c15acbf4774bff8b2a8ca93f82
Chiraitori <77224364+tathaha@users.noreply.github.com>
2024-03-15 10:12:00 +0700

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,27 +1,29 @@
 <template>
-  <footer class="bg-gray-800 text-gray-300 py-4 absolute bottom-0 w-full">
-    <div class="container mx-auto px-10 flex justify-between items-center">
-      <!-- Left side with copyright info -->
-      <div class="text-sm">
-        <p>&copy; {{ currentYear }} Vanilla</p>
-      </div>
+  <div class="wrapper">
+    <footer class="bg-gray-800 text-gray-300 py-4 w-full">
+      <div class="container mx-auto px-10 flex justify-between items-center">
+        <!-- Left side with copyright info -->
+        <div class="text-sm">
+          <p>&copy; {{ currentYear }} Your Company</p>
+        </div>
 
-      <!-- Middle section with developed by information -->
-      <div class="text-sm">
-        <p>
-          Developed By
-          <a href="https://chiraitori.lol" class="text-blue-400 hover:underline">Chiraitori</a>
-        </p>
-      </div>
+        <!-- Middle section with developed by information -->
+        <div class="text-sm">
+          <p>
+            Developed By
+            <a href="https://chiraitori.lol" class="text-blue-400 hover:underline">Chiraitori</a>
+          </p>
+        </div>
 
-      <!-- Right side with social icons -->
-      <div class="flex space-x-4">
-        <a href="#" class="text-gray-300 hover:text-white"><i class="fab fa-facebook"></i></a>
-        <a href="#" class="text-gray-300 hover:text-white"><i class="fab fa-twitter"></i></a>
-        <a href="#" class="text-gray-300 hover:text-white"><i class="fab fa-instagram"></i></a>
+        <!-- Right side with social icons -->
+        <div class="flex space-x-4">
+          <a href="#" class="text-gray-300 hover:text-white"><i class="fab fa-facebook"></i></a>
+          <a href="#" class="text-gray-300 hover:text-white"><i class="fab fa-twitter"></i></a>
+          <a href="#" class="text-gray-300 hover:text-white"><i class="fab fa-instagram"></i></a>
+        </div>
       </div>
-    </div>
-  </footer>
+    </footer>
+  </div>
 </template>
 
 <script>
@@ -42,4 +44,14 @@ export default {
 
 /* Example Font Awesome styles */
 @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css');
+
+.wrapper {
+  min-height: calc(100vh - 60px); /* Adjust 60px according to your header height */
+  display: flex;
+  flex-direction: column;
+}
+
+footer {
+  margin-top: auto;
+}
 </style>
